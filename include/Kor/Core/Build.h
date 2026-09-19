35ac6026fd0fc5fe7e548bfd0feb1972303da824
krisfis <ikrisfis@icloud.com>
2026-09-18 20:05:53 -0400

    NARG

--- include/Kor/Core/Build.h
+++ include/Kor/Core/Build.h
@@ -186,13 +186,54 @@
 // General-purpose utility macros. No platform or config dependencies.
 // -------------------------------------------------------------------------
 
+// Forces an extra rescan pass; needed to work around MSVC's legacy __VA_ARGS__ expansion bug.
 #define KOR_PP_EXPAND(x) x
 
+// Turns x into a string literal (optionally expands first)
 #define KOR_PP_STRINGIFY(x) #x
 #define KOR_PP_STRINGIFY_EXPAND(x) KOR_PP_STRINGIFY(x)
 
+// Pastes x and y into one token (optionally expands first)
 #define KOR_PP_CONCAT(x, y) x##y
 #define KOR_PP_CONCAT_EXPAND(x, y) KOR_PP_CONCAT(x,y)
 
+// Pastes three tokens together (optionally expands first)
 #define KOR_PP_CONCAT3(x, y, z) KOR_PP_CONCAT_EXPAND(KOR_PP_CONCAT_EXPAND(x, y), z)
 #define KOR_PP_CONCAT3_EXPAND(x, y, z) KOR_PP_CONCAT3(x, y, z)
+
+// Appends a unique per-expansion suffix to prefix (per-TU counter if available, else line number).
+#if defined(__COUNTER__)
+	#define KOR_PP_UNIQUE_NAME(prefix) KOR_PP_CONCAT_EXPAND(prefix, __COUNTER__)
+#else
+	#define KOR_PP_UNIQUE_NAME(prefix) KOR_PP_CONCAT_EXPAND(prefix, __LINE__)
+#endif
+
+// NARG \ ARG_NUM
+// Gets number of arguments passed to it
+// **IMPORTANT**: Number of args should be within 1 and 64
+// * Discovered by Jens Gustedt / Laurent Deniau
+// -------------------------------------------------------------------------
+
+#define KOR_PP_ARG_NUM(...) KOR_PP_NARG_IMPL(__VA_ARGS__, KOR_PP_RSEQ_N())
+#define KOR_PP_NARG(...) KOR_PP_NARG_IMPL(__VA_ARGS__, KOR_PP_RSEQ_N())
+
+// Re-splits __VA_ARGS__ before selection, working around MSVC's legacy expansion bug.
+#define KOR_PP_NARG_IMPL(...) KOR_PP_ARG_N_IMPL(__VA_ARGS__)
+
+#define KOR_PP_ARG_N_IMPL( \
+		_1, _2, _3, _4, _5, _6, _7, _8, _9,_10,	\
+		_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,\
+		_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,\
+		_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,\
+		_41,_42,_43,_44,_45,_46,_47,_48,_49,_50,\
+		_51,_52,_53,_54,_55,_56,_57,_58,_59,_60,\
+		_61,_62,_63,N,...) N
+
+#define KOR_PP_RSEQ_N_IMPL() \
+		63,62,61,60,					\
+		59,58,57,56,55,54,53,52,51,50,	\
+		49,48,47,46,45,44,43,42,41,40,	\
+		39,38,37,36,35,34,33,32,31,30,	\
+		29,28,27,26,25,24,23,22,21,20,	\
+		19,18,17,16,15,14,13,12,11,10,	\
+		9,8,7,6,5,4,3,2,1,0
